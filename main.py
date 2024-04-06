2c39a59693c1d1fad441d294640576e7ca82c2b9
yutasato88 <165874457+yutasato88@users.noreply.github.com>
2024-04-06 16:46:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-Get This Tools DM https://t.me/cahayaa878
+from src.cnc import start as start_cnc
+
+if __name__ == '__main__':
+    try:
+        start_cnc()
+    except Exception as e:
+        print(f"Error, skipping.  {e}")
